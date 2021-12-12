c7d6e78ca5e4925c76ad5d796771479364988029
ignatown <ignatow.n@mail.ru>
2021-12-12 03:41:44 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("list", List.of("One", "Two", "There", "Four"));
+        model.addAttribute("user", "Nick");
         return "index";
     }
 }
